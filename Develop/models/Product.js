6741432e7dc7a2946a6b21b1f01989b51d7992d0
Jessica Galarza <jessicaga941@gmail.com>
2021-10-27 19:37:41 -0400

    added database models

---
+++ Develop/models/Product.js
@@ -0,0 +1,60 @@
+// import important parts of sequelize library
+const { Model, DataTypes } = require('sequelize');
+// import our database connection from config.js
+const sequelize = require('../config/connection');
+
+// Initialize Product model (table) by extending off Sequelize's Model class
+class Product extends Model {}
+
+// set up fields and rules for Product model
+Product.init(
+  {
+    // define columns
+    id:{
+      type: DataType.INTEGER,
+      allowNull:false,
+      primaryKey:true,
+      autoIncrement:true,
+    },
+
+product_name:{
+      type: DataType.STRING,
+      allowNull:false,
+  },
+
+  price:{
+    type:DataType.DECIMAL,
+    allowNull:false,
+    validate:{
+      isDecimal:true,
+  },
+
+  stock:{
+    type:DataType.INTEGER,
+    allowNull:false,
+    len: [10], 
+    validate:{
+isNumeric:true,
+    },
+
+    category_id:{
+      type:DataType.INTEGER,
+      reference:{
+        model:"category",
+        key:"id"
+        
+      }
+    }
+  }
+},
+},
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product'
+  
+  });
+
+module.exports = Product;
---
+++ Develop/models/Tag.js
@@ -0,0 +1,31 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Tag extends Model {}
+
+Tag.init(
+  {
+    // define columns
+    id:{
+      dataType: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+
+    }, 
+    
+    tag_name:{
+      dataType: DataTypes.STRING,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'tag',
+  }
+);
+
+module.exports = Tag;
