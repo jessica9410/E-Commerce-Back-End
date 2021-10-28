dd7689c8cf36f402d92bc5cbd2982549a89a5e35
Jessica Galarza <jessicaga941@gmail.com>
2021-10-27 22:18:25 -0400

    added associations to model folder, index.js file

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,25 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsToCategory(Category,{
+  foreignKey:'category_id'
+});
+ 
 // Categories have many Products
-
+Category.hasManyProducts(Product,{
+  foreignKey:'category_id'
+});
 // Products belongToMany Tags (through ProductTag)
+Product.belongToManyTags(Tags, {
+  through:'ProductTag',
+  foreignKey:'product_id'
 
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongToManyProducts(Product, {
+  through:'ProductTag',
+  foreignKey:'tag_id'
+});
 
 module.exports = {
   Product,
